04d976c73c3d093a140ae7cb0dc8f5b624b7832c
Stefan Dyankov <s.dyankov@gmail.com>
2015-09-07 22:59:55 +0300

    Add colors

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 rm -rf .git
-echo 'Installing NPM dependencies...'
-sudo npm install --silent
-echo 'Installing Composer dependencies...'
+echo -e "\e[0;33mInstalling NPM dependencies...\e[0m"
+sudo npm install --silent > /dev/null 2>&1
+echo -e "\e[0;33mInstalling Composer dependencies...\e[0m"
 composer update -q
 echo '' > README.md
-echo 'Initializing new Git Repository...'
+echo -e "\e[0;33mInitializing new Git Repository...\e[0m"
 git init -q
 rm -- "$0"
-echo 'DONE!'
+echo -e "\e[0;32mDONE!\e[0m"
