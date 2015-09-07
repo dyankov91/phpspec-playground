7454480dc8aeb1a95705cb32113da604ff48d227
Stefan Dyankov <s.dyankov@gmail.com>
2015-09-07 23:10:47 +0300

    Update colors

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 rm -rf .git
-echo -e "\e[0;33mInstalling NPM dependencies...\e[0m"
+echo -e "\033[0;33mInstalling NPM dependencies...\033[0m"
 sudo npm install --silent > /dev/null 2>&1
-echo -e "\e[0;33mInstalling Composer dependencies...\e[0m"
+echo -e "\033[0;33mInstalling Composer dependencies...\033[0m"
 composer update -q
 echo '' > README.md
-echo -e "\e[0;33mInitializing new Git Repository...\e[0m"
+echo -e "\033[0;33mInitializing new Git Repository...\033[0m"
 git init -q
 rm -- "$0"
-echo -e "\e[0;32mDONE!\e[0m"
+echo -e "\033[0;32mDONE!\033[0m"
